88323459bffe09dea383d4da2752f8c2847baa7a
Nikita-Voloshko <65704273+Nikita-Voloshko@users.noreply.github.com>
2020-06-04 16:02:09 +0300

    Update Worker.py
    Исправленная запись.

--- Worker.py
+++ Worker.py
@@ -1,14 +1,29 @@
 class Worker():
-    name = 'Никита'
-    surname = 'Волошко'
-    position = 'Директор'
-    income = {"wage":22234, "bonus":345}
+    name = ''
+    surname = ''
+    position = ''
+    income = {}
+
+    def __init__(self, name, surname, position, income):
+        Worker.name = name
+        Worker.surname = surname
+        Worker.position = position
+        Worker.income = income
+    # name = 'Никита'
+    # surname = 'Волошко'
+    # position = 'Директор'
+    # income = {"wage":22234, "bonus":345}
+
+
 class Position(Worker):
     def get_full_name(self):
         return Worker.position + ' ' + Worker.surname + ' ' + Worker.name
+
     def get_total_income(self):
         return Worker.income["wage"] + Worker.income["bonus"]
 
-a = Position()
+
+a = Position('Никита', 'Волошко', 'директор', {"wage": 22234, "bonus": 345})
+a.__init__('Никита', 'Волошко', 'директор', {"wage": 22234, "bonus": 345})
 print(a.get_full_name())
 print('Зарплата', a.get_total_income(), 'рублей.')
