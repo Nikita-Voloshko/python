9512aaca7f38f335474392b1125ea67f08ffea68
Nikita-Voloshko <65704273+Nikita-Voloshko@users.noreply.github.com>
2020-06-04 15:49:22 +0300

    Update Road.py
    Исправленная версия.

--- Road.py
+++ Road.py
@@ -1,9 +1,13 @@
 class Road():
-    # length = input()
-    # width = input()
-    def __init__(length, width):
-        return float(length) * float(width) * 25 * 5 / 1000
+    length = 0
+    width = 0
+    def __init__(self, length, width):
+        Road.length = length
+        Road.width = width
+    def sum(self):
+        return float(Road.length) * float(Road.width) * 25 * 5 / 1000
 
 
 a = Road
-print(a.__init__(input('Введите длину '), input('Введите ширину ')), "тонн.")
+a.__init__(0, input('Введите длину '), input('Введите ширину '))
+print(a.sum(0), "тонн.")
